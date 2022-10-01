48412939ef30afe48e35662e513113eb39047b94
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-01 14:17:01 +0200

    feat : init files

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Components/Footer';
+import Header from '../Components/Header';
+
+const Home = () => {
+	return (
+		<div className='page'>
+			<Header />
+			<Footer />
+		</div>
+	);
+};
+
+export default Home;
