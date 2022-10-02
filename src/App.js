3d918782f4e1fba9aee99687c06a061b3e0aa309
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-02 15:16:56 +0200

    feat : route header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Error from "./Pages/Error";
 import About from "./Pages/About";
 import Apartments from "./Pages/Apartments";
+import Header from "./Components/Header";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/apartments/*" element={<Apartments />} />
         <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+      </Route>
+    </Routes>
   );
 };
 
