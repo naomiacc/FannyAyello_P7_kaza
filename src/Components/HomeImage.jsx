88123a4168f124a31883aee4bbae824dfcfa99e7
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-02 18:07:47 +0200

    feat : home container

---
+++ src/Components/HomeImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function HomeImage(props) {
+	return (
+		<section
+			className='homeImage__container'
+			style={{ backgroundImage: `url(${props.img})` }}>
+			<div className='homeImage__container__background'></div>
+			<h2 className='homeImage__container__title'>{props.title}</h2>
+		</section>
+	);
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
+import HomeImage from '../Components/HomeImage';
+import backgroundHome from '../Assets/background-home.png';
+
+const maskGroup = {
+	img: backgroundHome,
+	title: 'Chez vous, partout et ailleurs',
+};
+
 const Home = () => {
 	return (
 		<div className='page'>
 			<Header />
+			<div className='home container'>
+				<HomeImage {...maskGroup} />
+			</div>
 			<Footer />
 		</div>
 	);
