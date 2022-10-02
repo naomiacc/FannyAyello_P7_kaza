6475930cc2864dc1b5d253ece10eaeb1f9c0984c
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-02 15:17:36 +0200

    feat : improvements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./Styles/index.css";
+import { BrowserRouter } from "react-router-dom";
+
+import "./Styles/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
