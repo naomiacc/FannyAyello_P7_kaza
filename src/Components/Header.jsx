5109eae44722b58d19e496d35dc13cb0bab1ee02
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-02 15:16:35 +0200

    feat : header + delete files logo and nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import Logo from './header-logo';
-import Nav from './header-nav';
+import { NavLink } from 'react-router-dom';
+import logo from '../Assets/logo-home.png';
 
 function Header() {
 	return (
-		// Header
 		<header className='header'>
-			<Logo />
-			<Nav />
+			<h1 className='header__logo'>
+				<img src={logo} alt='kaza header logo' className='prim-on-sec' />
+			</h1>
+			<nav className='header__nav'>
+				<ul className='header__nav__ul'>
+					<li className='header__nav__ul__li'>
+						<NavLink to='/'>Accueil</NavLink>
+					</li>
+					<li className='header__nav__ul__li'>
+						<NavLink to='/about'>A propos</NavLink>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 }
