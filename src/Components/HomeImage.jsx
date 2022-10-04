c7ee4ee658b77e95ea7718794444699233f2baca
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-05 00:05:05 +0200

    feat : remove style props

--- src/Components/HomeImage.jsx
+++ src/Components/HomeImage.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function HomeImage(props) {
 	return (
 		<section
-			className='homeImage__container'
-			style={{ backgroundImage: `url(${props.img})` }}>
+			className='homeImage__container'>
+			{/* // style={{ backgroundImage: `url(${props.img})` }}> */}
 			<div className='homeImage__container__background'></div>
 			<h2 className='homeImage__container__title'>{props.title}</h2>
 		</section>
