97744f2a5d5dd8a8dd905170f244fec3abc3a901
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-05 00:04:20 +0200

    docs : renaming folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import "./Styles/index.scss";
+import "./styles/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
