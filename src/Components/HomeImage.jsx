af097f873c9e4a1171385245f0369c0038d2b47b
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-05 20:55:40 +0200

    feat : style homeImage

--- src/Components/HomeImage.jsx
+++ src/Components/HomeImage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../styles/sass/layout/_homeImage.scss';
+
 
 export default function HomeImage(props) {
 	return (
 		<section
-			className='homeImage__container'>
-			{/* // style={{ backgroundImage: `url(${props.img})` }}> */}
+			className='homeImage__container'
+			style={{ backgroundImage: `url(${props.img})` }}>
 			<div className='homeImage__container__background'></div>
 			<h2 className='homeImage__container__title'>{props.title}</h2>
 		</section>
