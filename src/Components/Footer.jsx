b9f624eac7f66d4c0e2a79c21d5a4dacb37730db
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-06 11:46:22 +0200

    feat : footer ok

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import logo from '../assets/logo-footer.png';
+import '../styles/sass/layout/_footer.scss';
+
 
 function Footer (props) {
 	return (
-		<p> Footer</p>
-
+		<footer className='footer white-on-black'>
+			<img src={logo} alt='kaza footer__logo' className='footer__logo' />			
+			<p className='footer__text'>&copy; 2020 Kaza. All rights reserved</p>
+		</footer>
 	)
 }
 
 export default Footer;
+
+
