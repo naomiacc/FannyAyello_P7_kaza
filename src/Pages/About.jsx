f3ceddb7db1c01d51903c28206116e8303919596
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-06 16:58:42 +0200

    feat : about page finished

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
+import HomeImage from '../components/HomeImage';
+import BackgroundAbout from '../assets/background-about.png';
+
+import dropDownData from '../assets/about.json';
+import DropDown from '../components/DropDown';
+
+const maskGroup = {
+	img: BackgroundAbout,
+	title: '',
+};
 
 function About (props) {
 	return (
-		<p> About</p>
-
-	)
+		<div className='page'>
+			<div className='about container'>
+				<HomeImage {...maskGroup} />
+				{dropDownData.map((object) => (
+					<DropDown {...object} key={dropDownData.indexOf(object)} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default About;
