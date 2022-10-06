305a46ccf7244b2713ec323abc926e67ba6bbdf5
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-06 13:56:09 +0200

    feat : page error finished

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/sass/pages/_error.scss';
 
 
 function Error (props) {
 	return (
-		<p> Error</p>
-
+		<div className='page'>
+			<div className='error container'>
+				<h1 className='error__h1'>404</h1>
+				<p className='error__p'>
+					Oups! La page que vous demandez n'éxiste pas
+				</p>
+				<Link to='/' className='error__link'>
+					Retourner sur la page d'accueil
+				</Link>
+			</div>
+		</div>
 	)
 }
 
