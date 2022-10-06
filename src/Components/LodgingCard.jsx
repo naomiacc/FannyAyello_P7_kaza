b774c74d2263844a15e1f0e261eb8ca42466ec9e
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-07 01:06:46 +0200

    feat : import link card

--- src/Components/LodgingCard.jsx
+++ src/Components/LodgingCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const LodgingCard = (props) => {
   return (
     <div className="lodgingCard">
-      {/* <Link to={"/apartments/?id=" + props.id} className="lodgingCard__link">
+      <Link to={"/apartments/?id=" + props.id} className="lodgingCard__link">
         <img
           src={props.pictures[0]}
           alt={props.title}
@@ -12,7 +12,7 @@ const LodgingCard = (props) => {
         ></img>
         <div className="lodgingCard__link__background"></div>
         <p className="lodgingCard__title">{props.title}</p>
-      </Link> */}
+      </Link>
     </div>
   );
 };
