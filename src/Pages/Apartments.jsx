0548cdc2669838f46ee0080c0c820b735c365dd1
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-10 19:49:20 +0200

    feat : import carroussel and description

--- src/Pages/Apartments.jsx
+++ src/Pages/Apartments.jsx
@@ -3,7 +3,12 @@ import Footer from "../Components/Footer"
 
 import data from '../Assets/logements.json';
 
+import Carroussel from '../Components/Carroussel';
+import Description from '../Components/Description';
 import Error from '../Pages/Error';
+
+import '../Styles/sass/pages/_apartments.scss';
+
 
 const Apartments = (props) => {
 	let pageId = new URLSearchParams(window.location.search).get('id');
@@ -12,9 +17,9 @@ const Apartments = (props) => {
 	return (
 		<div className='page'>
 			{found ? (
-				<div className='lodging container prim-on-sec'>
-					{/* {found.pictures ? <Carroussel {...found} /> : <h2>pas de photos</h2>}
-					<Description {...found} /> */}
+				<div className='apartments container prim-on-sec'>
+					{found.pictures ? <Carroussel {...found} /> : <h2>pas de photos</h2>}
+					<Description {...found} />
 				</div>
 			) : (
 				<Error />
