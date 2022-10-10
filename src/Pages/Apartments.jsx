6d99c0e5a9a4deb9a8a3e3a852e8b0f0e009d62e
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-10 18:25:15 +0200

    feat : pageId and container

--- src/Pages/Apartments.jsx
+++ src/Pages/Apartments.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import Footer from "../Components/Footer"
 
+import data from '../Assets/logements.json';
 
-function Apartments (props) {
+import Error from '../Pages/Error';
+
+const Apartments = (props) => {
+	let pageId = new URLSearchParams(window.location.search).get('id');
+	const found = data.find((element) => element.id === pageId);
+
 	return (
-		<p> Apartments</p>
+		<div className='page'>
+			{found ? (
+				<div className='lodging container prim-on-sec'>
+					{/* {found.pictures ? <Carroussel {...found} /> : <h2>pas de photos</h2>}
+					<Description {...found} /> */}
+				</div>
+			) : (
+				<Error />
+			)}
 
-	)
-}
+			<Footer />
+		</div>
+	);
+};
 
 export default Apartments;
