6cce5fde74b1ca719232407f93d14095443c5b28
naomiacc <fanny.ayello.campos@outlook.com>
2022-10-10 19:55:05 +0200

    feat : star scale apartment

---
+++ src/Components/StarScale.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import startFull from '../Assets/star-red.png';
+import starEmpty from '../Assets/star-white.png';
+const range = [1, 2, 3, 4, 5];
+
+const StarScale = (props) => {
+	let rating = props.rating;
+
+	return (
+		<div className='description__summary__right__rating'>
+			{range.map((rangeElement) =>
+				rating >= rangeElement ? (
+					<img
+						key={'star-Full' + rangeElement.toString()}
+						src={startFull}
+						alt='Star--full'
+						className='description__summary__right__rating__star'
+					/>
+				) : (
+					<img
+						key={'star-Empty' + rangeElement.toString()}
+						src={starEmpty}
+						alt='Star--full'
+						className='description__summary__right__rating__star'
+					/>
+				)
+			)}
+		</div>
+	);
+};
+
+export default StarScale;
